372cb57e921e5795b41b4931568f8a9a652a8626
Yasife <yasinsuniversity@gmail.com>
2025-10-03 13:19:19 +0330

    Create ControlPanel.py

---
+++ ControlPanel.py
@@ -0,0 +1,47 @@
+class control_panle:
+    
+    def __init__(self):
+        self.groups={}
+        
+    def create_group(self,group_name):
+        if group_name not in self.groups:
+            self.groups[group_name]=[]
+            print(f'groups {group_name} is created !!')
+            
+        else:
+            print('your group name is duplicated')
+
+  
+    def add_device_to_group(self,group_name,device):
+        if group_name in self.groups:
+            self.groups[group_name].append(device)
+            print(f'your devic is added to {group_name}')
+        else:
+            print('you group is not exist!!!')
+
+
+   def create_device(self,group_name,device_type,device_name):
+        
+        if group_name in self.groups:
+            location='home'
+            new_device=Device(location,group_name,device_type,device_name)
+            
+            self.groups[group_name].append(new_device)
+            print('your device is created successfully')
+            
+        else:
+            print('This device isnt exist!!') 
+
+
+   def create_multiple_device(self,group_name,device_type,device_number):
+        if group_name in self.groups:
+            
+            for i in range(1,device_number+1):
+                dv_name=f'{device_type}_{i}'
+                self.create_device(group_name,device_type,dv_name)
+
+            print(f'{device_number} devices created!!')
+            
+        else:
+            
+            print(f'{device_number} cant be create!!!')
